5f33a200dd99ac1866ca20ef916b7547f1b65a85
teymur <teymurhasanov100@gmail.com>
2023-12-27 11:43:14 +0400

    m

--- src/pages/Allletters.jsx
+++ src/pages/Allletters.jsx
@@ -115,9 +115,219 @@ const AllLetters = () => {
     setPageNumber(0)
   }
 
+  const customSelectStyles = {
+    placeholder: (baseStyles, state) => ({
+      ...baseStyles,
+      color: '#4a5568',
+    }),
+    control: (baseStyles, state) => ({
+      ...baseStyles,
+      borderColor: 'rgb(66, 153, 225)',
+      borderWidth: '2px',
+      borderRadius: '30px',
+      transition: 'border-color 0.2s ease',
+      backgroundColor: '#f6e05e',
+      '&:hover': {
+        borderColor: 'rgb(29, 78, 216)',
+      },
+    }),
+  }
+
   return (
-    <div className="mb-4 ml-6 mr-5 mt-4 flex-grow rounded-2xl border-2 border-darkblue-900 bg-gradient-to-r from-gray-300 to-gray-100 py-5 shadow-lg">
-      <div className="mb-1 border-b-2 border-gray-300 pb-1">
+    <div className="mb-4 ml-6 mr-5 mt-4 flex-grow rounded-2xl border-2 border-darkblue-900 bg-gradient-to-r from-gray-300 to-gray-100 py-2 shadow-lg">
+      <div className="mb-1 flex items-center justify-center gap-5 border-b-2 border-gray-300 pb-1">
+        <div className="ml-2 flex items-center justify-center rounded-lg border border-gray-500 bg-white px-2 py-2">
+          <span className="text-darkblue-400">Axtarış növü:</span>
+          <select
+            value={apiCall}
+            onChange={handleApiChange}
+            className="ml-1 rounded-lg border border-gray-500 bg-yellow-200 px-2 text-darkblue-400"
+          >
+            <option value="number">Məktubun &#8470;-si</option>
+            <option value="time">Tarix</option>
+            <option value="from">Haradan</option>
+            <option value="to">Hara</option>
+            <option value="created by">Müəllif</option>
+          </select>
+        </div>
+        {apiCall === 'number' && (
+          <div className="ml-2 flex items-center justify-center rounded-lg border border-gray-500 bg-white px-2 py-2">
+            <span className="text-darkblue-400">Məktublar:</span>
+            <input
+              type="number"
+              value={letterNoSearch}
+              placeholder="0"
+              onChange={(e) => setLetterNoSearch(e.target.value.replace(/\D/g, ''))}
+              className="ml-2 rounded-lg border border-gray-500 bg-yellow-200 pl-2 text-darkblue-400"
+            />
+            <button
+              onClick={() => setLetterNoSearch('')}
+              className="ml-4 rounded-lg border border-gray-500 bg-red-500 px-2 text-white"
+            >
+              Sil
+            </button>
+          </div>
+        )}
+        {apiCall === 'time' && (
+          <div className="ml-2 flex items-center justify-center rounded-lg border border-gray-500 bg-white px-2 py-2">
+            <span className="text-darkblue-400">Tarix:</span>
+            <input type="date" value={search} placeholder="search" onChange={handleSearchİnputChange} />
+            <button
+              onClick={() => setSearch('')}
+              className="ml-4 rounded-lg border border-gray-500 bg-red-500 px-2 text-white"
+            >
+              Sil
+            </button>
+          </div>
+        )}
+        {apiCall === 'from' && (
+          <div className="ml-2 flex items-center justify-center rounded-md">
+            <Select
+              options={departmentOptions}
+              onChange={(selectedOption) => {
+                if (selectedOption) {
+                  setFromDepartment(selectedOption.value)
+                  setSearch(selectedOption.value)
+                } else {
+                  setFromDepartment(null)
+                  setSearch('')
+                }
+              }}
+              value={fromDepartment ? { value: fromDepartment, label: fromDepartment } : null}
+              placeholder="Haradan"
+              styles={customSelectStyles}
+            />
+            <button
+              onClick={() => {
+                setFromDepartment(null)
+                setSearch('')
+              }}
+              className="ml-2 rounded-lg border border-gray-500 bg-red-500 px-2 text-white"
+            >
+              Sil
+            </button>
+          </div>
+        )}
+        {apiCall === 'to' && (
+          <div className="ml-2 flex items-center justify-center rounded-md">
+            <Select
+              options={departmentOptions}
+              onChange={(selectedOption) => {
+                if (selectedOption) {
+                  setToDepartment(selectedOption.value)
+                  setSearch(selectedOption.value)
+                } else {
+                  setToDepartment(null)
+                  setSearch('')
+                }
+              }}
+              value={toDepartment ? { value: toDepartment, label: toDepartment } : null}
+              placeholder="Hara"
+              styles={customSelectStyles}
+            />
+            <button
+              onClick={() => {
+                setToDepartment(null)
+                setSearch('')
+              }}
+              className="ml-2 rounded-lg border border-gray-500 bg-red-500 px-2 text-white"
+            >
+              Sil
+            </button>
+          </div>
+        )}
+        {apiCall === 'created by' && (
+          <div className="ml-2 flex items-center justify-center rounded-md">
+            <Select
+              options={userOptions}
+              onChange={(selectedOption) => {
+                if (selectedOption) {
+                  setUser(selectedOption.label)
+                  setSearch(`${selectedOption.label}`)
+                } else {
+                  setUser(null)
+                  setSearch('')
+                }
+              }}
+              value={user ? { value: user, label: user } : null}
+              placeholder="Müəllif"
+              styles={customSelectStyles}
+            />
+            <button
+              onClick={() => {
+                setUser(null)
+                setSearch('')
+              }}
+              className="ml-2 rounded-lg border border-gray-500 bg-red-500 px-2 text-white"
+            >
+              Sil
+            </button>
+          </div>
+        )}
+        <div className="ml-2 flex items-center justify-center rounded-lg border border-gray-500 bg-white px-2 py-2">
+          <span className="text-darkblue-400">Sırala:</span>
+          <select
+            value={sortBy}
+            onChange={handleSortChange}
+            className="ml-1 rounded-lg border border-gray-500  bg-yellow-200 px-2 text-darkblue-400"
+          >
+            <option value="letterNo">Məktubun &#8470;-si</option>
+            <option value="date">Tarix</option>
+          </select>
+        </div>
+      </div>
+      <div className="min-h-[37rem] bg-white">
+        <table className="max-h-60 w-full border-collapse overflow-y-scroll bg-white font-sans">
+          <thead>
+            <tr className="bg-yellow-200">
+              <th className="border border-gray-400 px-2 py-1">Məktubun &#8470;</th>
+              <th className="border border-gray-400 px-2 py-1">Haradan</th>
+              <th className="border border-gray-400 px-2 py-1">Hara</th>
+              <th className="border border-gray-400 px-2 py-1">Mühümlük</th>
+              <th className="border border-gray-400 px-1 py-0">Müəllif</th>
+              <th className="border border-gray-400 px-2 py-1">Tarix</th>
+              <th className="border border-gray-400 px-2 py-1">Qeyd</th>
+            </tr>
+          </thead>
+          <tbody>
+            {!letterNoSearch &&
+              letters?.content?.length &&
+              letters?.content?.map((letter, index) => (
+                <tr key={index}>
+                  <td className="border border-gray-400 px-1 py-0">{letter.letterNo}</td>
+                  <td className="border border-gray-400 px-1 py-0">{letter.fromDepartment.name}</td>
+                  <td className="border border-gray-400 px-1 py-0">{letter.toDepartment.name}</td>
+                  <td className="border border-gray-400 px-1 py-0">{letter.importanceDegree.name}</td>
+                  <td className="cursor-pointer border border-gray-400 px-1 py-0">
+                    {letter.createdBy.firstName} {letter.createdBy.lastName} {letter.createdBy.fatherName}
+                  </td>
+                  <td className="border border-gray-400 px-1 py-0">{letter.date}</td>
+                  <td className="border border-gray-400 px-1 py-0">{letter.note}</td>
+                </tr>
+              ))}
+            {!letterNoSearch && !letters?.content?.length && (
+              <tr>
+                <td>Tapilmadi</td>
+              </tr>
+            )}
+            {letterNoSearch && letterByNo?.letterNo && (
+              <tr>
+                <td className="border border-gray-400 px-1 py-0">{letterByNo.letterNo}</td>
+                <td className="border border-gray-400 px-1 py-0">{letterByNo.fromDepartment.name}</td>
+                <td className="border border-gray-400 px-1 py-0">{letterByNo.toDepartment.name}</td>
+                <td className="border border-gray-400 px-1 py-0">{letterByNo.importanceDegree.name}</td>
+                <td className="cursor-pointer border border-gray-400 px-1 py-0">
+                  {letterByNo.createdBy.firstName} {letterByNo.createdBy.lastName}{' '}
+                  {letterByNo.createdBy.fatherName}
+                </td>
+                <td className="border border-gray-400 px-1 py-0">{letterByNo.date}</td>
+                <td className="border border-gray-400 px-1 py-0">{letterByNo.note}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+      <div className="flex items-center justify-center gap-4 border-t-2 border-gray-500 pt-1">
         <button
           onClick={handlePreviousPage}
           className="focus:shadow-outline rounded bg-blue-800 px-2 py-1 font-bold text-white hover:bg-blue-900 focus:outline-none"
@@ -131,8 +341,6 @@ const AllLetters = () => {
         >
           <option value="5">5</option>
           <option value="10">10</option>
-          <option value="15">15</option>
-          <option value="20">20</option>
         </select>
         <button
           onClick={handleNextPage}
@@ -140,167 +348,7 @@ const AllLetters = () => {
         >
           Next
         </button>
-        Search type:
-        <select value={apiCall} onChange={handleApiChange}>
-          <option value="number">Based on letters number</option>
-          <option value="time">Based on time</option>
-          <option value="from">Based on From</option>
-          <option value="to">Based on To</option>
-          <option value="created by">Based on created By</option>
-        </select>
-        {apiCall === 'number' && (
-          <span>
-            Letters &#8470;
-            <input
-              type="number"
-              value={letterNoSearch}
-              placeholder="enter number"
-              onChange={(e) => setLetterNoSearch(e.target.value.replace(/\D/g, ''))}
-            />
-            <button onClick={() => setLetterNoSearch('')}>Clear</button>
-          </span>
-        )}
-        {apiCall === 'time' && (
-          <span>
-            Tarix:
-            <input type="date" value={search} placeholder="search" onChange={handleSearchİnputChange} />
-            <button onClick={() => setSearch('')}>Clear</button>
-          </span>
-        )}
-        {apiCall === 'from' && (
-          <span>
-            <Select
-              options={departmentOptions}
-              onChange={(selectedOption) => {
-                if (selectedOption) {
-                  setFromDepartment(selectedOption.value)
-                  setSearch(selectedOption.value)
-                } else {
-                  setFromDepartment(null)
-                  setSearch('')
-                }
-              }}
-              value={fromDepartment ? { value: fromDepartment, label: fromDepartment } : null}
-              placeholder="Select department"
-            />
-            <button
-              onClick={() => {
-                setFromDepartment(null)
-                setSearch('')
-              }}
-            >
-              Clear
-            </button>
-          </span>
-        )}
-        {apiCall === 'to' && (
-          <span>
-            <Select
-              options={departmentOptions}
-              onChange={(selectedOption) => {
-                if (selectedOption) {
-                  setToDepartment(selectedOption.value)
-                  setSearch(selectedOption.value)
-                } else {
-                  setToDepartment(null)
-                  setSearch('')
-                }
-              }}
-              value={toDepartment ? { value: toDepartment, label: toDepartment } : null}
-              placeholder="Select department"
-            />
-            <button
-              onClick={() => {
-                setToDepartment(null)
-                setSearch('')
-              }}
-            >
-              Clear
-            </button>
-          </span>
-        )}
-        {apiCall === 'created by' && (
-          <span>
-            <Select
-              options={userOptions}
-              onChange={(selectedOption) => {
-                if (selectedOption) {
-                  setUser(selectedOption.label)
-                  setSearch(`${selectedOption.label}`)
-                } else {
-                  setUser(null)
-                  setSearch('')
-                }
-              }}
-              value={user ? { value: user, label: user } : null}
-              placeholder="Select user"
-            />
-            <button
-              onClick={() => {
-                setUser(null)
-                setSearch('')
-              }}
-            >
-              Clear
-            </button>
-          </span>
-        )}
-        Sort by
-        <select value={sortBy} onChange={handleSortChange}>
-          <option value="letterNo">Letter number</option>
-          <option value="date">Date</option>
-        </select>
       </div>
-
-      <table className="max-h-60 w-full border-collapse overflow-y-scroll bg-white font-sans">
-        <thead>
-          <tr className="bg-yellow-200">
-            <th className="w-[9%] border border-gray-400 px-2 py-1">Məktubun &#8470;</th>
-            <th className="w-[6%] border border-gray-400 px-2 py-1">Haradan</th>
-            <th className="w-[6%] border border-gray-400 px-2 py-1">Hara</th>
-            <th className="w-[8%] border border-gray-400 px-2 py-1">Mühümlük</th>
-            <th className="w-[16%] border border-gray-400 px-1 py-0">Müəllif</th>
-            <th className="w-[8%] border border-gray-400 px-2 py-1">Tarix</th>
-            <th className="border border-gray-400 px-2 py-1">Qeyd</th>
-          </tr>
-        </thead>
-        <tbody>
-          {!letterNoSearch &&
-            letters?.content?.length &&
-            letters?.content?.map((letter, index) => (
-              <tr key={index}>
-                <td className="border border-gray-400 px-1 py-0">{letter.letterNo}</td>
-                <td className="border border-gray-400 px-1 py-0">{letter.fromDepartment.name}</td>
-                <td className="border border-gray-400 px-1 py-0">{letter.toDepartment.name}</td>
-                <td className="border border-gray-400 px-1 py-0">{letter.importanceDegree.name}</td>
-                <td className="cursor-pointer border border-gray-400 px-1 py-0">
-                  {letter.createdBy.firstName} {letter.createdBy.lastName} {letter.createdBy.fatherName}
-                </td>
-                <td className="border border-gray-400 px-1 py-0">{letter.date}</td>
-                <td className="border border-gray-400 px-1 py-0">{letter.note}</td>
-              </tr>
-            ))}
-          {!letterNoSearch && !letters?.content?.length && (
-            <tr>
-              <td>Tapilmadi</td>
-            </tr>
-          )}
-          {letterNoSearch && letterByNo?.letterNo && (
-            <tr>
-              <td className="border border-gray-400 px-1 py-0">{letterByNo.letterNo}</td>
-              <td className="border border-gray-400 px-1 py-0">{letterByNo.fromDepartment.name}</td>
-              <td className="border border-gray-400 px-1 py-0">{letterByNo.toDepartment.name}</td>
-              <td className="border border-gray-400 px-1 py-0">{letterByNo.importanceDegree.name}</td>
-              <td className="cursor-pointer border border-gray-400 px-1 py-0">
-                {letterByNo.createdBy.firstName} {letterByNo.createdBy.lastName}{' '}
-                {letterByNo.createdBy.fatherName}
-              </td>
-              <td className="border border-gray-400 px-1 py-0">{letterByNo.date}</td>
-              <td className="border border-gray-400 px-1 py-0">{letterByNo.note}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
     </div>
   )
 }
